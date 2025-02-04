bd29aca398178be32ca5dbe4918dedb617d751c2
athulcoder <athul.sin.cos.tan@gmail.com>
2025-02-04 07:22:23 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
   };
   const handleAppend = (value) => {
     let newVal = calculator.appendValue(value);
+    console.log(newVal);
     setScreen(newVal);
   };
 
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,9 +4,9 @@ class Calculator{
         this.display = "0"
     }
     appendValue(value){
-        if (this.display[0]=="0"){
-            this.display = this.display.slice(1);
-        }
+        // if (this.display[0]=="0"){
+        //     this.display = this.display.slice(1);
+        // }
         let newVal = this.display + value;
         this.display = newVal
 
